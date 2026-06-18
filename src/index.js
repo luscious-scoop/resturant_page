12b1a95a859065057be86fb5f848ca2d407c2fa2
Haseeb <ah085904@gmail.com>
2026-06-18 15:06:45 +0500

    boiler plate for about section and its styles

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import "./style.css";
 import { homeScreenController } from "./home.js";
 import { menuScreenController } from "./menu.js";
 
-const screenHandler = (function () {
+const screenHandler = function () {
 	const homeBtn = document.querySelector(".home-btn");
 	const menuBtn = document.querySelector(".menu-btn");
 	const aboutBtn = document.querySelector(".about-btn");
 
 	homeBtn.addEventListener("click", homeScreenController);
 	menuBtn.addEventListener("click", menuScreenController);
-})();
+};
 
 // merge fix
